75eb5ba6e6749d06260b85b69a539e505bffb2dc
pantshaswat <pantshaswat@gmail.com>
2025-05-20 12:46:05 +0545

    ADD: Nepali Documents Feature

--- models/models_init.py
+++ models/models_init.py
@@ -3,13 +3,17 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sentence_transformers import SentenceTransformer
 def init_models():
     # Load the model and tokenizer
-    model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+    model = SentenceTransformer('universalml/Nepali_Embedding_Model')
 
     # Initialize TF-IDF vectorizer
     tfidf_vectorizer = TfidfVectorizer(
-        stop_words='english',
+        # stop_words='english',
+        stop_words=None,
         ngram_range=(1, 2),
         max_features=10000
     )
     
     return model, tfidf_vectorizer
+
+
+# XLM-R (XLM-RoBERTa)
